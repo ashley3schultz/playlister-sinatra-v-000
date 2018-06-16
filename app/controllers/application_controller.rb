4b5cb0ceef6c5fe17a3a65156ea578b82ed9409f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-16 23:40:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,9 +28,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/songs/new' do
+    @song = Song.create(name: params["Name"])
     @artist = Artist.find_by(name: params["Artist Name"]) || Artist.create(name: params["Artist Name"])
     @genre = Genre.find_by(params[:genre]) || Genre.create(name: params["Genre Name"])
-    @song = Song.create(name: params["Name"])
+    #@song = Song.create(name: params["Name"])
     @song.genres << @genre
     @artist.songs << @song
     @song.save
