fe7db0e87d35dd5f23f37c061a74b3aeb7489d22
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-16 22:48:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,6 +28,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/songs/new' do
+    binding.pry
     #@artist = Artist.find_by(params[:artist]) || Artist.create(params[:song])
     #@genre = Genre.find_by(params[:genre]) || Genre.create(params[:genre])
     #@song = Song.find_by(params[:song]) || Song.create(params[:song])
