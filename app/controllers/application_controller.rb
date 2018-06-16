449cfd68a86e7ed9572a94d113f96079bb0d76a8
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-16 23:19:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,8 +29,8 @@ class ApplicationController < Sinatra::Base
 
   post '/songs/new' do
     @artist = Artist.find_by(name: params["Artist Name"]) || Artist.create(name: params["Artist Name"])
-    @genre = Genre.find_by(params[:genre]) || Genre.create(params["Genre Name"])
-    @song = Song.find_by(params["Name"]) || Song.create(params["Name"])
+    @genre = Genre.find_by(params[:genre]) || Genre.create(name: params["Genre Name"])
+    @song = Song.find_by(name: params["Name"]) || Song.create(name: params["Name"])
     binding.pry
     #redirect "/songs/'#{@song.slug}'"
   end
