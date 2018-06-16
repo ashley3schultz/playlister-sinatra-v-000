467cd4bd14f7610396888a43d349715e6c414745
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-16 22:32:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ binding.pry
     @artist = Artist.find_by(params[:artist]) || Artist.create(params[:song])
     @genre = Genre.find_by(params[:genre]) || Genre.create(params[:genre])
     @song = Song.find_by(params[:song]) || Song.create(params[:song])
-    
+
     redirect 'songs/"#{@song.id}"'
   end
 
