cbfdacde94eb0026e72c53caf984e05d8a3a62e9
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-17 00:42:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ class ApplicationController < Sinatra::Base
     @genre = Genre.find_by(params[:genre]) || Genre.create(name: params["Genre Name"])
     @song = Song.find_by_slug(params[:slug])
     @song.name = params["Name"]
-    @song.artist = @artist
+    @song.artist_id = @artist.id
     @song.genre_ids = @genre.id
     @song.save
     #update(name: params["Name"], artist: "#{@artist}", genres: @genre)
