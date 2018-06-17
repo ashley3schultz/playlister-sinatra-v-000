43a43ddefd52c89622148d71663399cf0dd3f238
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-17 00:31:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,8 @@ class ApplicationController < Sinatra::Base
   post '/songs/edit' do
     @artist = Artist.find_by(name: params["Artist Name"]) || Artist.create(name: params["Artist Name"])
     @genre = Genre.find_by(params[:genre]) || Genre.create(name: params["Genre Name"])
-    @song = Song.update(name: params["Name"], artist: "#{@artist}")
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(name: params["Name"], artist: "#{@artist}")
     @song.genres = @genre
     #@artist.songs << @song if !@artist.songs.include?(@song)
     #binding.pry
