3a5b8cfe75b228b67ff44b02438778269b15b172
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-17 00:18:03 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
     @artist = Artist.find_by(name: params["Artist Name"]) || Artist.create(name: params["Artist Name"])
     @genre = Genre.find_by(params[:genre]) || Genre.create(name: params["Genre Name"])
     @song = Song.update(name: params["Name"], artist: "#{@artist}")
-    @song.genres << @genre if !@song.genre_ids.include?(@genre.id)
+    @song.genres << @genre if !@song.genres.include?(@genre)
     @artist.songs << @song if !@artist.songs.include?(@song)
     redirect "/songs/#{@song.slug}"
   end
