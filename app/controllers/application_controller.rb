338cdd7a200c9992c872a771f08c6e14877e8e4e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-17 00:43:01 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,6 +46,7 @@ class ApplicationController < Sinatra::Base
     @genre = Genre.find_by(params[:genre]) || Genre.create(name: params["Genre Name"])
     @song = Song.find_by_slug(params[:slug])
     @song.name = params["Name"]
+    binding.pry
     @song.artist_id = @artist.id
     @song.genre_ids = @genre.id
     @song.save
